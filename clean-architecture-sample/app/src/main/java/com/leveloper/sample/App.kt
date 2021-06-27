5014fc5b0ab8a74181ffe901cdaa182f3df129f8
SangHyun Jeong <tkdgusl94@gmail.com>
2021-06-27 21:13:26 +0900

    기본 구조 잡음

---
+++ clean-architecture-sample/app/src/main/java/com/leveloper/sample/App.kt
@@ -0,0 +1,7 @@
+package com.leveloper.sample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- clean-architecture-sample/presentation/src/main/java/com/leveloper/presentation/MainActivity.kt
+++ clean-architecture-sample/presentation/src/main/java/com/leveloper/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.leveloper.sample
+package com.leveloper.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
