001e138bac88f9dd5cf9a4a51c94dc0183530826
정상현 <tkdgusl94@mailplug.co.kr>
2021-07-01 11:54:40 +0900

    오픈소스 라이선스 라이브러리 완료

--- open-source-license/app/src/main/java/com/leveloper/open_source_license/MainActivity.kt
+++ open-source-license/app/src/main/java/com/leveloper/open_source_license/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.leveloper.open_source_license
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_opensource_license)?.let {
+            it.setOnClickListener {
+                startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+                OssLicensesMenuActivity.setActivityTitle("오픈소스 라이선스")
+            }
+        }
     }
 }
